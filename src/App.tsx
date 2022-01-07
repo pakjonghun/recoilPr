6ecbbe0a4af316ad9024480836455f62bef45435
정훈박 <fireking5997@gmail.com>
2022-01-08 03:52:53 +0900

    Add:가짜 데이터 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Draggable, Droppable, DragDropContext } from "react-beautiful-dnd";
 
+const list = [1, 2, 3, 4, 5, 6];
+
 function App() {
   const onDragEnd = () => {};
   return (
@@ -13,31 +15,34 @@ function App() {
               className="rounded-md p-3 w-full max-w-xs flex flex-col items-center justify-center bg-stone-200"
             >
               <h1 className=" mb-4 text-4xl font-bold">Fint</h1>
-              <Draggable index={1} draggableId="drag1">
-                {(provider) => (
-                  <li
-                    ref={provider.innerRef}
-                    {...provider.dragHandleProps}
-                    {...provider.draggableProps}
-                    className="flex cursor-pointer bg-red-200 w-full h-22 rounded-xl shadow-md mb-4"
-                  >
-                    <img
-                      className="m-5 mr-8 w-10 h-10 rounded-full shadow-md"
-                      src="https://images.unsplash.com/photo-1641499414064-306123010a9b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"
-                      alt="img"
-                    />
-                    <div className=" p-3 flex justify-between flex-col">
-                      <h2>Hellow my name is good</h2>
-                      <div className=" flex items-center mt-3 justify-between">
-                        <button className=" text-sm cursor-pointer rounded-md bg-blue-300 p-1">
-                          Fin
-                        </button>{" "}
-                        <small>id:1</small>
+              {list.map((i, x) => (
+                <Draggable key={x} index={x} draggableId={`drop${x}`}>
+                  {(provider) => (
+                    <li
+                      ref={provider.innerRef}
+                      {...provider.dragHandleProps}
+                      {...provider.draggableProps}
+                      className="flex cursor-pointer bg-red-200 w-full h-22 rounded-xl shadow-md mb-4"
+                    >
+                      <img
+                        className="m-5 mr-8 w-10 h-10 rounded-full shadow-md"
+                        src="https://images.unsplash.com/photo-1641499414064-306123010a9b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"
+                        alt="img"
+                      />
+                      <div className=" p-3 flex justify-between flex-col">
+                        <h2>{i}</h2>
+                        <div className=" flex items-center mt-3 justify-between">
+                          <button className=" text-sm cursor-pointer rounded-md bg-blue-300 p-1">
+                            Fin
+                          </button>
+                          <small>id:1</small>
+                        </div>
                       </div>
-                    </div>
-                  </li>
-                )}
-              </Draggable>
+                    </li>
+                  )}
+                </Draggable>
+              ))}
+              {provider.placeholder}
             </ul>
           )}
         </Droppable>
